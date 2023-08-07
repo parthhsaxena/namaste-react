d9043c03b3ba565ce7d9652281a75ad3194e63f4
parthhsaxena <parthsxn7@gmail.com>
2023-08-07 14:27:21 +0530

    abc

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
-const parent = React.createElement(
-  "div",
-  { id: "parent" },
-  React.createElement(
-    "div",
-    { id: "child" },
-    React.createElement("h1", {}, "I'm an h1 tag")
-  )
-);
+// Creating 2 children and heading siblings
+const parent = React.createElement("div", { id: "parent" }, [
+  React.createElement("div", { id: "child" }, [
+    React.createElement("h1", {}, "I'm an h1 tag"),
+    React.createElement("h1", {}, "I'm an h1 tag"),
+  ]),
+  React.createElement("div", { id: "child" }, [
+    React.createElement("h1", {}, "I'm an h1 tag"),
+    React.createElement("h1", {}, "I'm an h1 tag"),
+  ]),
+]);
 
 // createElement takes 3 arguments:- tagName, attributes, Children
 const heading = React.createElement(
@@ -17,5 +19,5 @@ const heading = React.createElement(
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-// render method will convert createElement object to heading tag and render it on DOM.
+// render method will convert createElement object to HTML and render it on DOM.
 root.render(parent);
