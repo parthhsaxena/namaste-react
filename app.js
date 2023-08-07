31c7e76cc8ffd751a1ff9b232f176692fec9caff
Parth Saxena <parthsaxena@Parths-MacBook-Pro.local>
2023-08-07 13:05:36 +0530

    episode-01 Code

---
+++ app.js
@@ -0,0 +1,21 @@
+const parent = React.createElement(
+  "div",
+  { id: "parent" },
+  React.createElement(
+    "div",
+    { id: "child" },
+    React.createElement("h1", {}, "I'm an h1 tag")
+  )
+);
+
+// createElement takes 3 arguments:- tagName, attributes, Children
+const heading = React.createElement(
+  "h1",
+  { id: "head", xyz: "abc" },
+  "Hello World from React!"
+);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+// render method will convert createElement object to heading tag and render it on DOM.
+root.render(parent);
