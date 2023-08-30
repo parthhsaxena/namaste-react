c2a56a9c9ef0fcc97ec39d798013c7db98cffb80
parthhsaxena <parthsxn7@gmail.com>
2023-08-30 07:56:38 +0530

    new commit

--- readme.md
+++ readme.md
@@ -1,3 +1,21 @@
 # Namaste React practice file
 
+# App Structure
 
+/\*\*
+
+- Header
+- - Logo
+- - Nav Items
+- Body
+- - Search
+- - Restaurant Container
+- - RestaurantCard
+-      - Image
+-      - Name, StarRating, Cuisines, DeliveryTime
+- Footer
+- - Copyright
+- - Links
+- - Address
+- - Contact
+    \*/
---
+++ src/components/Body.js
@@ -0,0 +1,45 @@
+import { useEffect, useState } from "react";
+import RestaurantCard from "./RestaurantCard";
+import resList from "../utils/mockData";
+
+const Body = () => {
+  const [listOfRestaurants, setListOfRestaurants] = useState([]);
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const fetchData = async () => {
+    const data = await fetch(
+      "https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.9715987&lng=77.5945627&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
+    );
+
+    const json = await data.json();
+    console.log(json);
+  };
+
+  return (
+    <div className="body">
+      <div className="filter">
+        <button
+          className="filter-btn"
+          onClick={() => {
+            const filteredList = listOfRestaurants.filter(
+              (res) => res.avgRating > 4
+            );
+            setListOfRestaurants(filteredList);
+          }}
+        >
+          Top Rated Restaurants
+        </button>
+      </div>
+      <div className="res-container">
+        {listOfRestaurants.map((restaurant) => (
+          <RestaurantCard key={restaurant.id} resData={restaurant} />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Body;
